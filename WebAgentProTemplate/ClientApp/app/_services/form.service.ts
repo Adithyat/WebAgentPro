ba72040060d4e6ec8882b183a2b0cfcd3e1a2b7c
A. A. Thiruvalluvan <a.a.thiruvalluvan@avanade.com>
2019-08-05 11:33:57 -0700

    cleaning up code

--- WebAgentProTemplate/ClientApp/app/_services/form.service.ts
+++ WebAgentProTemplate/ClientApp/app/_services/form.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { QuoteReceipt } from '../_models/quotereceipt';
-import { Driver } from '../_models/driver';
 
 @Injectable({
   providedIn: 'root'
